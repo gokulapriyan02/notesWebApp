a13b78b4c5a1051c7faa99ad67baa18450ccde09
gokulapriyan02 <gokulapriyan02@gmail.com>
2026-06-29 14:15:18 +0530

    feat: basic functionality

--- frontend/src/Note.jsx
+++ frontend/src/Note.jsx
@@ -1,8 +1,43 @@
+import { useState } from "react";
+import NoteItems from "./NoteItems";
 function Note(){
+
+const [note, setNote] = useState({
+    title: "",
+    content: ""
+});
+
+const [noteItems, setNoteItems] = useState([]);
+
+function storeNote(event){
+    const {name, value} = event.target;
+    setNote((prev) => ({...prev, [name]: value}) );
+
+
+}
+
+function addNote(){
+    setNoteItems((prev) => ([...prev, note]));
+    setNote({title:"", content:""});
+}
+
+
     return(
         <div>
             <h1>Handy Notes</h1>
             <p>Write, Edit, Access at Anytime, Anywhere..!</p>
+            <div>
+                <h2>Write what's on your Mind!</h2>
+                <input type="text" name="title" onChange={storeNote} value={note.title}/>
+                <input type="text"  name="content" onChange={storeNote} value={note.content}/>
+                <button onClick={addNote}>Add</button>
+            </div>
+            <div>
+                {noteItems.map((item, index) =>{
+                     return  <NoteItems title={item.title} content={item.content} key={index}/>
+                })}
+               
+            </div>
         </div>
     );
 }
---
+++ frontend/src/NoteItems.jsx
@@ -0,0 +1,12 @@
+function NoteItems({title, content, index}){
+
+return (
+    <div>
+        <h1>{title}</h1>
+        <p>{content}</p>
+    </div>
+);
+
+}
+
+export default NoteItems;
