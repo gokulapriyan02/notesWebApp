1227bedd5461951ff0d0454aec70269c324644bb
gokulapriyan02 <gokulapriyan02@gmail.com>
2026-07-01 23:47:38 +0530

    added header, footer sections

---
+++ frontend/src/footer.jsx
@@ -0,0 +1,11 @@
+function Footer (){
+
+
+    return ( <footer>
+                    <p>Copyright</p>
+                    <h1>Footer</h1>
+              </footer>
+              
+            );
+}
+export default Footer
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import reactDOM  from "react-dom/client";
 import Note from "./Note.jsx"
-
+import Footer from "./footer.jsx"
 
 reactDOM.createRoot(document.getElementById("root")).render(
     <StrictMode>
          <Note />
+         <Footer />
     </StrictMode>
 )
